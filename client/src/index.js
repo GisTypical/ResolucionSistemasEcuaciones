71eee9851a2e4d6682fb8643aac6ada78724f4da
Gerardo <gerardopapaleco@gmail.com>
2021-02-27 14:00:33 -0400

    deleted .git in /client

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Help from './Help';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Help />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
